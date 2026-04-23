a99db5e04e4e8e9ad6cb446b391991f74510f44d
TselmuunUranchimeg <tselmuunu@gmail.com>
2026-04-23 17:07:21 +0800

    Attempt #1 of fixing to find .env files through Telescope

--- init.lua
+++ init.lua
@@ -440,6 +440,7 @@ require('lazy').setup({
         pickers = {
           find_files = {
             hidden = true,
+            no_ignore = true,
           },
         },
         extensions = {
